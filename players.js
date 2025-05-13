7fcec27ddf09e232ab70bcee887e07c19de55198
hayley-exe <526hslater@frhsd.com>
2025-05-13 09:31:31 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,149 @@
+const players = [
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        godlyParent: `later`,
+        seriesAppearance: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `later`,
+        lastName: `later`,
+        parent: `later`,
+        series: `later`,
+        photo: `https://picsum.photos/200`
+    },
+]
