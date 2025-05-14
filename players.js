209322bc9c67919bbe8b29418b5df4fbb482ebf5
hayley-exe <526hslater@frhsd.com>
2025-05-14 11:54:29 -0400

    so it begins and my sanity ends

--- players.js
+++ players.js
@@ -1,149 +1,262 @@
 const players = [
     {
+        firstName: `Percy`,
+        lastName: `Jackson`,
+        heritage: `Son of Poseidon`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Annabeth`,
+        lastName: `Chase`,
+        heritage: `Daughter of Athena`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Grover`,
+        lastName: `Underwood`,
+        heritage: `Saytr, Lord of the Wild`,
+        series: `Percy Jackson and the Olympians, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Luke`,
+        lastName: `Castellan`,
+        heritage: `Son of Hermes, Host of Kronos`,
+        series: `Percy Jackson and the Olympians`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Thalia`,
+        lastName: `Grace`,
+        heritage: `Daughter of Zeus, Hunter of Artemis`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Nico`,
+        lastName: `Di Angelo`,
+        heritage: `Son of Hades`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Bianca`,
+        lastName: `Di Angelo`,
+        heritage: `Daughter of Hades`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Rachel`,
+        lastName: `Elizabeth Dare`,
+        heritage: `Mortal, Oracle of Delphi`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Clarisse`,
+        lastName: `La Rue`,
+        heritage: `Daughter of Ares`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Chiron`,
+        lastName: `Trainer of Heroes`,
+        heritage: `Centaur, Camp Director`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Dionysus`,
+        lastName: `"Mr. D"`,
+        heritage: `God, Camp Director`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Will`,
+        lastName: `Solace`,
+        heritage: `Son of Apollo`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Ethan`,
+        lastName: `Nakamura`,
+        heritage: `Son of Nemesis`,
+        series: `Percy Jackson and the Olympians`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Travis`,
+        lastName: `Stoll`,
+        heritage: `Son of Hermes`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Connor`,
+        lastName: `Stoll`,
+        heritage: `Son of Hermes`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Silena`,
+        lastName: `Beauregard`,
+        heritage: `Daughter of Aphrodite`,
+        series: `Percy Jackson and the Olympians`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Charles “Charlie”`,
+        lastName: `Beckendorf`,
+        heritage: `Son of Hepheastus`,
+        series: `Percy Jackson and the Olympians`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Jason`,
+        lastName: `Grace`,
+        heritage: `Son of Jupiter`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Piper`,
+        lastName: `McLean`,
+        heritage: `Daughter of Aphrodite`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Leo`,
+        lastName: `Valdez`,
+        heritage: `Son of Hepheastus`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Festus`,
+        lastName: `Argo II Figurehead`,
+        heritage: `Mechanical Dragon`,
+        series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Frank`,
+        lastName: `Zhang`,
+        heritage: `Son of Mars`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Hazel`,
+        lastName: `Levesque`,
+        heritage: `Daughter of Pluto`,
+        series: `Heroes of Olympus`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Reyna Avila`,
+        lastName: `Ramírez-Arellano`,
+        heritage: `Daughter of Bellona`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Gleeson`,
+        lastName: `Hedge`,
+        heritage: `Satyr`,
+        series: `Heroes of Olympus, Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Octavian`,
+        lastName: `Augur`,
+        heritage: `Legacy of Apollo`,
+        series: `Heroes of Olympus`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Magnus`,
+        lastName: `Chase`,
+        heritage: `Son of Frey`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `"Jack" Sumarbrander`,
+        lastName: ``,
+        heritage: `Sword of Summer`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Samirah`,
+        lastName: `al-Abbas`,
+        heritage: `Daughter of Loki`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Alex`,
+        lastName: `Fierro`,
+        heritage: `Child of Loki`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Blitzen`,
+        lastName: ``,
+        heritage: `Dwarf from Nidavellir`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Hearthstone`,
+        lastName: ``,
+        heritage: `Elf from Alfheim`,
+        series: `Magnus Chase and the Gods of Asgard`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Lester`,
+        lastName: `Papadopoulos`,
+        heritage: `Apollo in mortal form`,
+        series: `Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Meg`,
+        lastName: `McCaffery`,
+        heritage: `Daughter of Demeter`,
+        series: `Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+    {
+        firstName: `Kayla`,
+        lastName: `Knowles`,
+        heritage: `Daughter of Apollo`,
+        series: `Trials of Apollo`,
+        photo: `https://picsum.photos/200`
+    },
+
+    {
         firstName: `later`,
         lastName: `later`,
-        parent: `later`,
+        heritage: `later`,
         series: `later`,
         photo: `https://picsum.photos/200`
     },
     {
         firstName: `later`,
         lastName: `later`,
-        parent: `later`,
+        heritage: `later`,
         series: `later`,
         photo: `https://picsum.photos/200`
     },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        godlyParent: `later`,
-        seriesAppearance: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-    {
-        firstName: `later`,
-        lastName: `later`,
-        parent: `later`,
-        series: `later`,
-        photo: `https://picsum.photos/200`
-    },
-]
+];
--- roster.js
+++ roster.js
@@ -1,45 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const grid = document.getElementById('rosterGrid')
+    const grid = document.getElementById('rosterGrid');
 
     const render = list => {
-        grid.innerHTML = ''
+        grid.innerHTML = '';
 
         list.forEach(p => {
+            const col = document.createElement('div');
+            col.className = 'col-6 col-lg-2';
 
-            const col = document.createElement('div')
-
-            col.className = 'col-6 col-lg-2'
             col.innerHTML = `
-    <div class="card h-100 shadow-sm">
-        <img src="${p.photo}" class="card-img-top" alt="${p.firstName} ${p.lastName}">
-    <div class="card-body text-center">
-        <h5 class="card-title mb-1">
-          ${p.firstName} ${p.lastName}
-    </h5>
-    <div class="badge badge-parent badge-pos-${p.parent}"
-    ${p.parent}
-    </div>
-    <p class="small text-muted mb-0">
-    series ${p.series}
-    </p>
-        </div>
-    </div>`
+                <div class="card h-100 shadow-sm">
+                    <img src="${p.photo || 'https://via.placeholder.com/150'}" class="card-img-top" alt="${p.firstName} ${p.lastName}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
+                         <p class="small text-muted mb-0">Heritage: ${p.heritage}</p>
+                        <p class="small text-muted mb-0">Series: ${p.series}</p>
+                        
+                    </div>
+                </div>
+            `;
 
-            grid.appendChild(col)
+            grid.appendChild(col);
+        });
+    };
 
+    render(players); // Ensure `players` is defined before this runs
 
-
-        })
-
-
-
-
-    }
-
-    render(players)
-
-
-
-
-})
+});
