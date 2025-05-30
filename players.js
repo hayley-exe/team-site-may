6e6577544fddbb40f25187f63d8553156223a71e
hayley-exe <526hslater@frhsd.com>
2025-05-30 11:45:45 -0400

    COMPLETE!!!!!!!! probably

--- players.js
+++ players.js
@@ -6,8 +6,8 @@ const players = [
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//PercyJackson.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
-        info: `Percy is the brave and often sarcastic son of Poseidon.He’s best known for saving Olympus—more than once—and can control water.
-Fun fact: He once defeated a Titan with a wave and a well- timed punch.`
+        info: `Percy is the loyal and often sarcastic son of Poseidon.He’s best known for saving Olympus, multiple times, and (unwilling) doing quests for the gods.
+Fun fact: He jumped into the equivalent of literal hell after Annabeth was dragged in.`
     },
     {
         firstName: `Annabeth`,
@@ -17,7 +17,7 @@ Fun fact: He once defeated a Titan with a wave and a well- timed punch.`
         photo: `imgs//AnnabethChase.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`],
         info: `Annabeth is a brilliant strategist, daughter of Athena, and scary smart and beautiful girlfriend of Percy Jackson. She’s fearless in battle and determined to succeed .
-Fun fact: She re-designed Olympus for it to be rebuilt after it was almost entirely destroyed.`
+Fun fact: She re-designed Olympus for it to be rebuilt after the Battle of Manhattan.`
     },
     {
         firstName: `Grover`,
@@ -27,7 +27,7 @@ Fun fact: She re-designed Olympus for it to be rebuilt after it was almost entir
         photo: `imgs//GroverUnderwood.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
         info: `A satyr with a huge heart, Grover is Percy’s best friend and protector. He's also the Lord of the Wild, chosen by Pan himself.
-Fun fact: He can talk to animals and trees—and they listen! He also charmed a horde of snakes on the pan pipes and his last resort song was "Stayin' Alive". `
+Fun fact:He once charmed a horde of snakes on the pan pipes and when running out of time, his last resort was to play the song "Stayin' Alive". `
     },
     {
         firstName: `Luke`,
@@ -37,7 +37,7 @@ Fun fact: He can talk to animals and trees—and they listen! He also charmed a 
         photo: `imgs//LukeCastellan.jpg`,
         badges: [`imgs//pjo.png`],
         info: `Once a trusted son of Hermes, Luke became the host of Kronos. His story is one of betrayal, redemption, and sacrifice.
-Fun fact: He ultimately chose to destroy Kronos from within and willing went out to receive the curse of Achilles.`
+Fun fact: Even after hosting Kronos, he went to Annabeth asking to run away together and avoid the gods and his fate before Kronos could fully take over..`
     },
     {
         firstName: `Thalia`,
@@ -56,7 +56,7 @@ Fun fact: He ultimately chose to destroy Kronos from within and willing went out
         photo: `imgs//Tyson.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`,],
         info: `Tyson is a sweet, strong Cyclops and Percy's half-brother. Despite being underestimated, he’s a war hero and forge master.
-Fun fact: He loves peanut butter and trained battle hippocampi, his favorite one he named 'Rainbow'`
+Fun fact: He loves peanut butter and used it as a battle cry.`
     },
     {
         firstName: `Travis and Connor`,
@@ -65,7 +65,7 @@ Fun fact: He loves peanut butter and trained battle hippocampi, his favorite one
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//StollBrothers.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
-        info: `The Stoll brothers are mischievous sons of Hermes and serial pranksters, despite looking like twins they are actually a year apart in age. Fun Fact: They managed to uproot and replant live landmines, no one knows how they did that but it definitely helped in the battle against the Romans.`
+        info: `The Stoll brothers are mischievous sons of Hermes and serial pranksters, despite looking like twins they are actually a year apart in age. Fun Fact: They managed to uproot and replant live landmines, no one knows how they did it, and it's probably best not to ask.`
     },
     {
         firstName: `Charles “Charlie”`,
@@ -113,7 +113,7 @@ Fun fact: She sacrificed herself to save her friends from a mechanical giant and
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//Zoe.jpg`,
         badges: [`imgs//pjo.png`],
-        info: `A loyal Hunter of Artemis, Zoë was once a Hesperide. She died battling the monster Ladon during the Titan war.
+        info: `A loyal Hunter of Artemis, Zoë was once a Hesperide. She died from the poison of the monster Ladon at the hands of father, Atlas.
 Fun fact: She harbored great hatred towards Heracules and Percy was the only man she actually liked as she literally said "Perhaps not all men" in regards to him.`
     },
     {
@@ -123,7 +123,7 @@ Fun fact: She harbored great hatred towards Heracules and Percy was the only man
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//RachelDare.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
-        info: `Rachel is a mortal who can see through the Mist and became the Oracle of Delphi. She helped guide heroes with cryptic prophecies.
+        info: `Rachel is a mortal who can see through the Mist and became the Oracle of Delphi. With her artistic abilities and prophetic dreams, she paints prophecies as well as speaks them.
 Fun fact: She hit Kronos, Lord of the Titans, in the eye with a blue plastic hairbrush.`
     },
     {
@@ -133,8 +133,8 @@ Fun fact: She hit Kronos, Lord of the Titans, in the eye with a blue plastic hai
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//Calypso.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
-        info: `Calypso is the immortal daughter of Atlas, cursed to be alone on Ogygia. She eventually finds freedom and love with Leo Valdez.
-Fun fact: She has centuries of wisdom—and makes excellent healing potions, upon Leo's arrival to her island she asked the gods 'are you kidding!'.`
+        info: `Calypso is the immortal daughter of Atlas, cursed to be alone on Ogygia and fall in love with every hero who shows up and then leaves forever. She eventually finds freedom and love with Leo Valdez.
+Fun fact: Upon Leo's arrival to her island she screamed at the gods 'are you kidding!'.`
     },
     {
         firstName: `Will`,
@@ -143,8 +143,8 @@ Fun fact: She has centuries of wisdom—and makes excellent healing potions, upo
         series: `Percy Jackson and the Olympians, Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//WillSolace.jpg`,
         badges: [`imgs//pjo.png`, `imgs//hoo.png`, `imgs//toa.png`],
-        info: `Will is a calm and competent son of Apollo and a talented healer. He’s Nico's boyfriend and balances his darkness with light.
-Fun fact: Percy grabbed him out of the Battle of Manhattan, on a scooter. He also delivered a baby satyr during the war with the Romans.`
+        info: `Will is the son of Apollo and a talented healer, he is Nico's boyfriend and was one of the few people not scared of him and literally yelled at him about his health.
+Fun fact: Percy grabbed him out of the Battle of Manhattan, on a scooter to heal Annabeth. He also delivered a baby satyr during the war with the Romans.`
     },
     {
         firstName: `Ethan`,
@@ -185,7 +185,7 @@ Fun fact: He could summon lightning and fly, he was Thalia's younger brother and
         photo: `imgs//PiperMcLean.jpg`,
         badges: [`imgs//hoo.png`, `imgs//toa.png`],
         info: `Piper is a daughter of Aphrodite with charm and courage. She often challenged the idea of what a "love goddess's daughter" should be.
-Fun fact: She used charmspeak to take cars for 'test drives' at a BMW dealership, she had no permit or license.`
+Fun fact: She used charmspeak to take cars for 'test drives' at BMW dealerships and acquired a magic cornucopia and uses it as a weapon, successfully on multiple occasions.`
     },
     {
         firstName: `Leo`,
@@ -194,8 +194,8 @@ Fun fact: She used charmspeak to take cars for 'test drives' at a BMW dealership
         series: `Heroes of Olympus, Trials of Apollo`,
         photo: `imgs//LeoValdez.jpg`,
         badges: [`imgs//hoo.png`, `imgs//toa.png`],
-        info: `Leo, a son of Hephaestus, is the team's mechanic and comic relief. He can light himself on fire restored the mechanical dragon, Festus, and when Festus was almost destroyed, he had him become figured of the flying giant warship he made from scratch (He also turned back into a dragon later and for some reason acn turn into a suitcase).
-Fun fact: He literally died and came back to life, he is known for his nicknames of both himself and for others.`
+        info: `Leo, a son of Hephaestus, is the team's mechanic and comic relief. He can light himself on fire and the proud creator of Festus, mechanical dragon and figurehead of flying giant warship he made from scratch (He also turned back into a dragon later and for some reason and turn into a rolling suitcase).
+Fun fact: He literally died and came back to life, and visited Ogyia twice, both of these things deemed impossible. As Apollo himself had said, "That should have been physically impossible, of course, but like any decent god, demigod, or engineer, Leo Valdez refused to be stopped by the laws of physics."`
     },
     {
         firstName: `Frank`,
@@ -215,7 +215,7 @@ Fun fact: His life is tied small piece of firewood and he once iguana-ed his way
         photo: `imgs//HazelLevesque.jpg`,
         badges: [`imgs//hoo.png`],
         info: `Hazel is a daughter of Pluto who returned from the dead. She can manipulate precious metals and underground terrain.
-Fun fact: Her horse, Arion, runs faster than sound, eats gold nuggets, and curse like a sailor (according to Percy because he can speak horse).`
+Fun fact: Her horse, Arion, runs faster than sound, eats gold nuggets, and curse like a sailor (according to Percy Jackson because as the son of Poseidon he can speak horse).`
     },
     {
         firstName: `Reyna Avila`,
@@ -235,7 +235,7 @@ Fun fact: She rode Pegasus across the sea to deliver the Athena Parthenos sister
         photo: `imgs//CoachHedge.jpg`,
         badges: [`imgs//hoo.png`, `imgs//toa.png`],
         info: `Coach Hedge is a loud, aggressive satyr who loves combat. He’s unexpectedly sentimental and deeply cares for his students.
-Fun fact: He keeps a baseball bat strapped to his back at all times and calls the demigods "cupcakes".`
+Fun fact: He keeps a baseball bat strapped to his back at all times and calls the demigods "cupcakes". Leo made his megaphone randomly yell things like 'the cow says moo!'`
     },
     {
         firstName: `Octavian`,
@@ -245,7 +245,7 @@ Fun fact: He keeps a baseball bat strapped to his back at all times and calls th
         photo: `imgs//Octavian.jpg`,
         badges: [`imgs//hoo.png`],
         info: `A Roman legacy of Apollo, Octavian was manipulative and power-hungry. He used prophecies for personal gain.
-Fun fact: He launched himself via catapult to be "the hero",it did not end well. He kinda was the one who ended the war, but not really and not heroically at all.`
+Fun fact: He launched himself via catapult to be "the hero",it did not end well. He would murder pillow pets for 'sacrifices' to the gods. (RIP to Percy's panda pillow pet, you will be missed)`
     },
     {
         firstName: `Magnus`,
@@ -254,8 +254,9 @@ Fun fact: He launched himself via catapult to be "the hero",it did not end well.
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//MagnusChase.jpg`,
         badges: ['imgs//mgatgoa.png'],
-        info: `Magnus is a son of Frey who died—and woke up in Valhalla. He wields a talking sword and prefers peace to violence.
-Fun fact: Whatever powerful things Jack does while wielding him, upon Jack turning back to pendant form the exhaustion falls upon him.`
+        info: `Magnus is a son of Frey who died—and woke up in Valhalla. He wields a talking sword and prefers peace to violence. He is a powerful healer and boyfriend of Alex Fierro.
+Fun fact: When kissed by Alex in male form he literally thought, "A small part of my brain thought, Alex is male right now. I have just been kissed by a dude. How do I feel about that?
+The rest of my brain answered: I have just been kissed by Alex Fierro. I am absolutely great with that."`
     },
     {
         firstName: `"Jack" Sumarbrander`,
@@ -275,7 +276,7 @@ Fun fact: He can fly and fight completely on his own, also pretty blunt and not 
         photo: `imgs//Samirah.jpg`,
         badges: ['imgs//mgatgoa.png'],
         info: `Sam is a Valkyrie and daughter of Loki who balances hero work with school and fasting during Ramadan.
-Fun fact: She flies into battle wearing a magical hijab and tricks her way out of .`
+Fun fact: Her last name means lion.`
     },
     {
         firstName: `Alex`,
@@ -283,7 +284,10 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Einherji, Child of Loki`,
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//Alex.jpg`,
-        badges: ['imgs//mgatgoa.png']
+        badges: ['imgs//mgatgoa.png'],
+        info: `Alex Fierro is a fierce, sarcastic, shape-shifting child of Loki who wields a killer garrote, shreds gender norms, and has zero tolerance for your nonsense.
+Fun Fact: Preferred weapon of choice is a razor-sharp ceramic-cutting wire, they have used it to decapitate someone, don't ask.
+`
     },
     {
         firstName: `Blitzen`,
@@ -291,7 +295,10 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Dwarf from Nidavellir`,
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//Blitzen.jpg`,
-        badges: ['imgs//mgatgoa.png']
+        badges: ['imgs//mgatgoa.png'],
+        info: `Blitzen is a fashionable dwarf and son of Freya. He hates traditional armor—but can forge magical items.
+Fun fact: He helped reforge Thor’s hammer with style and forged a rubber duck in a competition due to nervousness – this has happened more then once.
+`
     },
     {
         firstName: `Hearthstone`,
@@ -299,16 +306,20 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Elf from Alfheim`,
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//Hearthstone.jpg`,
-        badges: ['imgs//mgatgoa.png']
+        badges: ['imgs//mgatgoa.png'],
+        info: `Hearth is a deaf elf and powerful rune magician. He communicates through ASL and ancient runes.
+Fun fact: He turned down being cured of his deafness so he could pursue study of runes and magic.`
     },
 
     {
         firstName: `Mallory`,
         lastName: `Keen`,
-        heritage: `Einherji`,
+        heritage: `Einherji, Daughter of Frigg`,
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//MalloryKeen.jpg`,
-        badges: ['imgs//mgatgoa.png']
+        badges: ['imgs//mgatgoa.png'],
+        info: `Mallory is a fiery Irish Einjherji with a tragic past and a big heart. She’s fiercely loyal to her friends.
+Fun fact: She died while trying to disarm a car bomb during Bloody Friday.`
     },
     {
         firstName: `TJ`,
@@ -316,7 +327,9 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Einherji, Son of Tyr`,
         series: `Magnus Chase and the Gods of Asgard`,
         photo: `imgs//TJ.jpg`,
-        badges: ['imgs//mgatgoa.png']
+        badges: ['imgs//mgatgoa.png'],
+        info: `TJ is an African-American son of Tyr and a Union soldier from the Civil War. He fights with honor and a big smile.
+Fun fact: Due to Britain's decision not to support the Confederates, he is very grateful to English people and constantly thanks everyone he meets in York.`
     },
     {
         firstName: `Lester`,
@@ -324,7 +337,10 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Apollo in mortal form`,
         series: `Trials of Apollo`,
         photo: `imgs//Lester.jpg`,
-        badges: [`imgs//toa.png`]
+        badges: [`imgs//toa.png`],
+        info: `Once the god Apollo, Lester is Zeus’s punishment in mortal form. He must learn humility, empathy, and bravery.
+Fun fact: Used playing "Tiptoe Through the Tulips" by Tiny Tim on the ukulele as a threat,it was very affective.
+`
     },
     {
         firstName: `Meg`,
@@ -332,7 +348,9 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Daughter of Demeter`,
         series: `Trials of Apollo`,
         photo: `imgs//Meg.jpg`,
-        badges: [`imgs//toa.png`]
+        badges: [`imgs//toa.png`],
+        info: `Meg is a fierce daughter of Demeter and companion of Lester. She wields twin golden sickles and loves plants.
+Fun fact: She summoned a deadly army of peaches—yes, peaches.`
     },
     {
         firstName: `Kayla`,
@@ -340,6 +358,8 @@ Fun fact: She flies into battle wearing a magical hijab and tricks her way out o
         heritage: `Daughter of Apollo`,
         series: `Trials of Apollo`,
         photo: `imgs//Kayla.jpg`,
-        badges: [`imgs//toa.png`]
+        badges: [`imgs//toa.png`],
+        info: `Kayla is a daughter of Apollo and Canadian archery instructor Darren Knowles. She is a year round camper who dreams to compete in the Olympics
+Fun fact: Currently the only known demigod whose parents, both god and mortal, are the same gender. We don’t question it nor are we surprised considering the very queer nature of the Greek Gods.`
     },
 ];
